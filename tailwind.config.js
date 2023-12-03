fcb65c8af3a3aba635effba3107b703c6a94447d
KameJu <sasiskasssss6052@gmail.com>
2023-12-03 21:28:34 +0200

    start

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import  colors  from 'tailwindcss/colors'
+
+const constants = {
+	primary: '#F3F3F3',
+	gray: '#5F5F5F',
+	'dark-gray': '#505050',
+	black: '#181818'
+}
+
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		colors: { transparent: colors.transparent, ...constants },
+		extend: {}
+	},
+	plugins: []
+}
